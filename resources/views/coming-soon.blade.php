9caaba5d22aa9e1acdc7027a3c1c3aaeb16ebdd7
Ruzman <roesman.labfik@umi.ac.id>
2022-06-29 18:19:10 +0800

    inisialisasi

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+
+<!-- =========================================================
+* Frest - Bootstrap Admin Template | v1.0.0
+==============================================================
+
+* Product Page: https://1.envato.market/frest_admin
+* Created by: PIXINVENT
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright PIXINVENT (https://pixinvent.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html lang="en" class="light-style " dir="ltr" data-theme="theme-default" data-assets-path="assetsdash/" data-template="horizontal-menu-template">
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Maintenance</title>
+
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 admin, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://1.envato.market/frest_admin">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assetsdash/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="assetsdash/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="assetsdash/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="assetsdash/vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="assetsdash/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="assetsdash/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="assetsdash/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="assetsdash/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="assetsdash/vendor/libs/typeahead-js/typeahead.css" />
+
+
+    <!-- Page CSS -->
+    <!-- Page -->
+<link rel="stylesheet" href="assetsdash/vendor/css/pages/page-misc.css">
+    <!-- Helpers -->
+    <script src="assetsdash/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="assetsdash/vendor/js/template-customizer.js"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="assetsdash/js/config.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="async" src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'GA_MEASUREMENT_ID');
+    </script>
+    <!-- Custom notification for demo -->
+    <!-- beautify ignore:end -->
+
+</head>
+
+<body>
+
+  <!-- Content -->
+
+<!--Under Maintenance -->
+<div class="container-xxl container-p-y">
+  <div class="misc-wrapper">
+    <h1 class="mb-2 mx-2">Gomennasai!</h1>
+    <p class="mb-4 mx-2">
+      Maaf yah gaes, kami harus perbaiki web dlu persiapan MPLS😚
+    </p>
+    <a href="https://www.instagram.com/smuneljc/" class="btn btn-primary">Instagram</a>
+    <div class="mt-4">
+      <img src="assetsdash/img/illustrations/itsukyodaibanner.png" alt="girl-doing-yoga-light" width="500" class="img-fluid" data-app-light-img="illustrations/itsukyodaibanner.png" data-app-dark-img="illustrations/boy-working-dark.png">
+    </div>
+  </div>
+</div>
+<!-- /Under Maintenance -->
+
+<!-- / Content -->
+
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  <script src="assetsdash/vendor/libs/jquery/jquery.js"></script>
+  <script src="assetsdash/vendor/libs/popper/popper.js"></script>
+  <script src="assetsdash/vendor/js/bootstrap.js"></script>
+  <script src="assetsdash/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <script src="assetsdash/vendor/libs/hammer/hammer.js"></script>
+
+
+  <script src="assetsdash/vendor/libs/i18n/i18n.js"></script>
+  <script src="assetsdash/vendor/libs/typeahead-js/typeahead.js"></script>
+
+  <script src="assetsdash/vendor/js/menu.js"></script>
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+
+
+
+  <!-- Main JS -->
+  <script src="assetsdash/js/main.js"></script>
+
+  <!-- Page JS -->
+
+
+</body>
+
+</html>

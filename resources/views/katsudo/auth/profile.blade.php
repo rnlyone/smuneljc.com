e9a3778bae0cd1aa9e8083b8ca5cbd02b2b9c123
Ruzman <roesman.labfik@umi.ac.id>
2026-07-08 08:41:57 +0900

    welcome setting

--- resources/views/katsudo/auth/profile.blade.php
+++ resources/views/katsudo/auth/profile.blade.php
@@ -53,7 +53,7 @@
                 @endif
             </picture>
             <!-- Status badge (small, floating bottom-right) -->
-            <div style="position:absolute;bottom:-6px;right:-6px;width:40px;height:40px;display:flex;align-items:center;justify-content:center;">
+            <div style="position:absolute;bottom:-10px;right:-10px;width:50px;height:50px;display:flex;align-items:center;justify-content:center;">
                 @if(auth('pendaftar')->user()->dpt->img)
                     <img src="/images/{{auth('pendaftar')->user()->dpt->img}}" alt="Departemen" style="width:100%;height:100%;object-fit:contain;">
                 @else
